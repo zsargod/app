a084fb0df970c09f25e337eeee96b412f8709e03
David Zsargo <dzsargo@scissero.com>
2026-03-26 19:22:29 +0100

    Added essentials

---
+++ js/modules/app/main.mjs
@@ -0,0 +1,16 @@
+import Alpine from '../lib/alpinejs.mjs';
+
+import '../directive/destroy.mjs';
+import '../directive/ancor.mjs';
+
+import '../data/import.mjs';
+
+import '../magic/template.mjs';
+import '../magic/log.mjs';
+import '../magic/stringify.mjs';
+import '../magic/assign.mjs';
+import '../magic/wait.mjs';
+import '../magic/emit.mjs';
+import '../magic/origin.mjs';
+
+export default Alpine;
